105c2ab57f1584918efeec1fc2db0721b1c129d9
Eric Jizba <erijiz@microsoft.com>
2024-09-09 11:44:13 -0700

    init

---
+++ src/functions/httpTrigger1.js
@@ -0,0 +1,15 @@
+const { app } = require('@azure/functions');
+
+app.http('httpTrigger1', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const name = request.query.get('name') || await request.text() || 'world';
+
+        return { body: `Hello, ${name}!` };
+    }
+});
+
+throw new Error('Oops!')
